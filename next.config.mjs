4cb79947f4810f5b55c2ba4254aa577b08a3e3c6
Nishanth <nishanth230499@gmail.com>
2025-02-10 18:59:35 -0800

    removed duplicates in graphs

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,11 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   output: 'export',
-  basePath: '/graph-benchmark',
+  basePath: '/graph-benchmark-website',
   images: {
     unoptimized: true,
   },
-  assetPrefix: isProd ? '/graph-benchmark/' : '',
+  assetPrefix: isProd ? '/graph-benchmark-website/' : '',
 }
 
 export default nextConfig
--- src/graphs/index.js
+++ src/graphs/index.js
@@ -1,13 +1,11 @@
 import binGraphs from './bin.json'
 import knnGraphs from './knn.json'
-import linksGraphs from './links.json'
 import pbbsGraphs from './pbbs.json'
 import syntheticGraphs from './synthetic.json'
 
 const graphs = {
   ...binGraphs,
   ...knnGraphs,
-  ...linksGraphs,
   ...pbbsGraphs,
   ...syntheticGraphs,
 }
