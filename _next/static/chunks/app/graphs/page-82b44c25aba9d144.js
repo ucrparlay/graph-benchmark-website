bdee55be0730f6fe0ac3a50691aa376f3b4926ae
Nishanth <nishanth230499@gmail.com>
2025-02-10 16:03:00 -0800

    added next js implementation

---
+++ _next/static/chunks/app/graphs/page-82b44c25aba9d144.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[866],{},_=>{var e=e=>_(_.s=e);_.O(0,[173,547,441,517,358],()=>e(8547)),_N_E=_.O()}]);
--- _next/static/chunks/app/graphs/page-dc49e69ebbc58f8b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[866],{},_=>{var e=e=>_(_.s=e);_.O(0,[173,815,441,517,358],()=>e(4815)),_N_E=_.O()}]);
